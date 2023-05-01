9122d4a20a7e99919a70bd54625ece3d1b45ed2c
RJ <rjgilo2799@gmail.com>
2023-05-01 14:42:13 -0400

    added render base URL

--- e-commerce-frontend/src/services/axios.js
+++ e-commerce-frontend/src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://flexwearfitness.onrender.com/",
 });
 
 export default instance;
